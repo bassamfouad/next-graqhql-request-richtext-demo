a2917d51ca0fa348f5524c0b3a8e3e090ac521ed
bassamfouad <b.foaud@cyclus.digital>
2023-02-06 14:10:41 +0200

    worked on feedback --- created new repo--- init project boilerplate + added query to fetch data based on static slug

---
+++ src/pages/[slug].tsx
@@ -0,0 +1,41 @@
+import { client } from "@/client";
+import { RichTextRenderer } from "@caisy/rich-text-react-renderer";
+import { gql } from "graphql-request";
+export default function Home({ data }) {
+  return (
+    <>
+      <h1>{data?.title}</h1>
+      <RichTextRenderer node={data?.text.json} />
+    </>
+  );
+}
+export async function getStaticProps({ params }) {
+  const gqlResponse = await client.request(
+    gql`
+      query allBlogArticle($slug: String) {
+        allBlogArticle(where: { slug: { eq: $slug } }) {
+          edges {
+            node {
+              text {
+                json
+              }
+              title
+              slug
+              id
+            }
+          }
+        }
+      }
+    `,
+    { slug: params?.slug }
+  );
+  return {
+    props: { data: gqlResponse?.allBlogArticle?.edges?.[0]?.node }, // will be passed to the page component as props
+  };
+}
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: true,
+  };
+}
